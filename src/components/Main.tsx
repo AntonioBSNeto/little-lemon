1aff9c457cfc7a2e1cf9771502f09c551b1c5cdc
Antonio Neto <absn3@cin.ufpe.br>
2024-12-10 09:35:21 -0300

    feat: adding page main

---
+++ src/components/Main.tsx
@@ -0,0 +1,11 @@
+import { Hero } from "./Hero"
+import { Highlights } from "./Highlights"
+
+export const Main = () => {
+  return (
+    <main>
+      <Hero />
+      <Highlights />
+    </main>
+  )
+}
